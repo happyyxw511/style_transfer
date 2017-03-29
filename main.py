caa36dc14e1a67ab0f3f5d5508280e965dc2fb94
happyyxw511 <happyyxw511@gmail.com>
2017-03-28 21:56:38 -0700

    refine the gpu configuration setup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import tensorflow as tf
 import src.utils as utils
 import src.loss_calculation as loss_calculation
 import src.train as train
@@ -12,11 +12,6 @@ style_weight = 1e2
 tv_weight = 2e2
 style_image = utils.get_img(style_image_path)
 
-style_loss = loss_calculation.style_and_content_loss(style_image,
-                                                     vgg_model_path,
-                                                     content_weight,
-                                                     style_weight,
-                                                     tv_weight)
 def _get_files(img_dir):
     files = utils.list_files(img_dir)
     return [os.path.join(img_dir,x) for x in files]
@@ -27,5 +22,15 @@ try:
 except Exception:
     pass
 
-content_image_paths = _get_files(content_image_path)
-train.optimize(style_loss, content_image_paths, shoud_print=True)
+run_config = tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)
+run_config.gpu_options.allow_growth=True
+with tf.Session(config=run_config) as sess:
+    style_loss = loss_calculation.style_and_content_loss(
+        sess,
+        style_image,
+        vgg_model_path,
+        content_weight,
+        style_weight,
+        tv_weight)
+    content_image_paths = _get_files(content_image_path)
+    train.optimize(sess, style_loss, content_image_paths, shoud_print=True)
--- src/train.py
+++ src/train.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 import src.utils as utils
 
-def optimize(loss,
+def optimize(sess,
+             loss,
              content_image_paths,
              batch_size=4,
              learning_rate=1e-3,
@@ -10,29 +11,30 @@ def optimize(loss,
              shoud_print=False,
              save_path='saver/fns.ckpt'):
     batch_shape = (batch_size,256,256,3)
-    with tf.device('/gpu:0'), tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-        optimizer = tf.train.AdadeltaOptimizer(learning_rate).minimize(loss)
-        sess.run(tf.global_variables_initializer())
-        for epoch in xrange(epochs):
-            num_examples = len(content_image_paths)
-            np.random.shuffle(content_image_paths)
-            print content_image_paths[:10]
-            iterations = 0
-            infered_losses = []
-            # the input should have been randomized
-            while iterations * batch_size < num_examples:
-                curr = iterations*batch_size
-                step = curr + batch_size
-                X_batch = np.zeros(batch_shape, dtype=np.float32)
-                for j, path in enumerate(content_image_paths[curr: step]):
-                    X_batch[j] = utils.get_img(path, img_size=(256, 256, 3)).astype(np.float32)
-                iterations += 1
-                optimizer.run(feed_dict={'X_content_images:0': X_batch})
-                saver = tf.train.Saver()
-                saver.save(sess, save_path)
-                if shoud_print:
-                    inferred_loss = sess.run([loss], feed_dict={'X_content_images:0': X_batch})
-                    infered_losses.append(inferred_loss)
-                    if np.mod(iterations, 10) == 0:
-                        print np.mean(inferred_loss)
-                        infered_losses = []
+
+
+    optimizer = tf.train.AdadeltaOptimizer(learning_rate).minimize(loss)
+    sess.run(tf.global_variables_initializer())
+    for epoch in xrange(epochs):
+        num_examples = len(content_image_paths)
+        np.random.shuffle(content_image_paths)
+        print content_image_paths[:10]
+        iterations = 0
+        infered_losses = []
+        # the input should have been randomized
+        while iterations * batch_size < num_examples:
+            curr = iterations*batch_size
+            step = curr + batch_size
+            X_batch = np.zeros(batch_shape, dtype=np.float32)
+            for j, path in enumerate(content_image_paths[curr: step]):
+                X_batch[j] = utils.get_img(path, img_size=(256, 256, 3)).astype(np.float32)
+            iterations += 1
+            optimizer.run(feed_dict={'X_content_images:0': X_batch})
+            saver = tf.train.Saver()
+            saver.save(sess, save_path)
+            if shoud_print:
+                inferred_loss = sess.run([loss], feed_dict={'X_content_images:0': X_batch})
+                infered_losses.append(inferred_loss)
+                if np.mod(iterations, 10) == 0:
+                    print np.mean(inferred_loss)
+                    infered_losses = []
